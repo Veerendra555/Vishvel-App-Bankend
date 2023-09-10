244ffb02687879d7074b21e8d6dff3bc29b1c100
charuvikram <charu.vikram@gmail.com>
2023-09-10 22:43:50 +0530

    code push

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
+        console.log(req.headers)
         let token = req.headers.authorization.split(" ")[1];
+        console.log(token);
         const decode = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decode;
         // req.query.userid = req.user._id;
