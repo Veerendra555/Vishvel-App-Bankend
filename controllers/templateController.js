5e7258f412a08705114095d13ce5fa0b0397243c
charuvikram <charu.vikram@gmail.com>
2023-09-16 14:43:18 +0530

    code push

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const Template = require('../models/template');
-
+const userDetail = require('../models/userdetail');
+const cheerio = require('cheerio');
+const fs = require('fs')
+const templateList=['card1.svg']
 class TemplateController {
 	constructor() {}
 
@@ -31,6 +34,123 @@ class TemplateController {
 						msg: `${err}`,
 					})
 				);
+		});
+	}
+
+	getCards(req) {
+		console.log("get Cards Calling..")
+		return new Promise((resolve, reject) => {
+			console.log("get Cards Calling..1")
+			try {
+			userDetail.findOne({userid:req.params.userid})
+				.then((data) => {
+					if (data.length == 0) {
+						resolve({
+							code: 204,
+							msg: 'No template found!!!',
+						});
+					} else {
+						console.log(data,"data==,>")
+						let newTemplateList = [];
+						for(let i=0;i<templateList.length;i++)
+						{
+						fs.readFile(`${__dirname}/../public/cards/${templateList[i]}`, 'utf8', (err, svgData) => {
+						 let finalData =	svgData.replace(/User Address/g, data.address )
+						                    .replace(/UserEmail/g, data.email)
+											.replace(/UserPhone/g, data.mob_no)
+											.replace(/Designation/g, data.occupation)
+											.replace(/UserName/g, data.name);
+							console.log("New SVG ==>",finalData);
+							if (err) {
+							  console.error(err);
+							//   return res.status(500).send('Error reading SVG file');
+													reject({
+										code: 204,
+										msg: 'No template found!!!',
+									});
+							}
+						
+							// Modify the SVG data (example: change the color of all <circle> elements)
+							// const $ = cheerio.load(svgData);
+							newTemplateList.push(svg)
+							// $('circle').attr('fill', 'red'); // Change circle fill color to red
+						
+							// Send the modified SVG data to the client
+							// const modifiedSVG = $.xml();
+							// console.log("New Svg==>",modifiedSVG);
+
+							// res.set('Content-Type', 'image/svg+xml');
+							// res.send(modifiedSVG);
+						  });	
+						  resolve({
+							code: 200,
+							result: newTemplateList,
+						});
+						} 
+						// resolve({
+						// 	code: 200,
+						// 	result: data,
+						// });
+					}
+				})
+				.catch((err) =>
+					reject({
+						code: 500,
+						msg: `${err}`,
+					})
+				);			
+			} catch (error) {
+				console.log(error);
+				reject({
+					code: 204,
+					msg: 'No template found!!!',
+				});
+			}
+			// var svgs = [];
+			// let links = [""]
+			// async.eachSeries(links, function(link, next) {
+			//   fs.readFile(link, function(err, svg) {
+			// 	svgs.push(svg);
+			// 	next(err);
+			//   });
+			// }, function(err) {
+			//   if (err) {
+			// 							resolve({
+			// 				code: 204,
+			// 				msg: 'No template found!!!',
+			// 			});
+			// 	// response.writeHead(500);
+			// 	// response.end(JSON.stringify(err));
+			// 	// return;
+			//   }
+			//   					resolve({
+			// 				code: 200,
+			// 				result: JSON.stringify(svgs),
+			// 			});
+			// //   response.writeHead(200);
+			// //   response.end(svgs); // Doesn't work
+			//   // response.end(svgs[0]); // Works
+			// });
+			// Template.find(query)
+			// 	.then((data) => {
+			// 		if (data.length == 0) {
+			// 			resolve({
+			// 				code: 204,
+			// 				msg: 'No template found!!!',
+			// 			});
+			// 		} else {
+			// 			resolve({
+			// 				code: 200,
+			// 				result: data,
+			// 			});
+			// 		}
+			// 	})
+			// 	.catch((err) =>
+			// 		reject({
+			// 			code: 500,
+			// 			msg: `${err}`,
+			// 		})
+			// 	);
 		});
 	}
 
---
+++ models/usermessage.js
@@ -0,0 +1,25 @@
+const mongoose = require("../db/db");
+var ObjectId = mongoose.Schema.Types.ObjectId;
+// const bcrypt = require('bcrypt');
+
+// let saltRounds = 10;
+
+const MessageSchema = mongoose.Schema({
+        message: {
+          text: { type: String, required: true },
+        },
+        users: Array,
+        sender: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+      },
+      {
+        timestamps: true,
+      });
+
+const message = mongoose.model("Messages", MessageSchema);
+
+module.exports = message;
+
